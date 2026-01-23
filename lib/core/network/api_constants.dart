49eb6604ec828abbf15b2cb2e5591d078d6f9fe0
hainampp <hainamtoeic2k5@gmail.com>
2026-01-23 22:48:06 +0700

    production server

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -3,7 +3,7 @@ class ApiConstants {
   ApiConstants._();
 
   // Base URL - Change this according to your environment
-  static const String baseUrl = 'http://localhost:8000';
+  static const String baseUrl = 'http://http://14.225.218.83:8005';
 
   // Auth Endpoints
   static const String login = '/api/auth/login';
